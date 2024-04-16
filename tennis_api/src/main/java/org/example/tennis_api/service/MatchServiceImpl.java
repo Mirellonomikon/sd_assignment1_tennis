c7ac0f829b722cd40d8a3e9619f67d9f75e4a6fe
ZetsubouNate <mirelacalinescu2000@yahoo.com>
2024-04-17 01:00:00 +0300

    ye

--- tennis_api/src/main/java/org/example/tennis_api/service/MatchServiceImpl.java
+++ tennis_api/src/main/java/org/example/tennis_api/service/MatchServiceImpl.java
@@ -1,4 +1,19 @@
 package org.example.tennis_api.service;
 
-public class MatchServiceImpl {
+import org.example.tennis_api.mapper.MatchMapper;
+import org.example.tennis_api.repository.MatchRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MatchServiceImpl implements MatchService{
+
+    private final MatchRepository matchRepository;
+    private final MatchMapper matchMapper;
+
+    @Autowired
+    public MatchServiceImpl(MatchRepository matchRepository, MatchMapper matchMapper) {
+        this.matchRepository = matchRepository;
+        this.matchMapper = matchMapper;
+    }
 }
